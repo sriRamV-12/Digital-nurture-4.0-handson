3cd62e3deb3d7ed9c93a8d26eb1615a703974fea
sriRamV-12 <sriram202004@gmail.com>
2025-07-05 20:31:35 +0530

    Added JavaFSE-week-3 with initial files

---
+++ JavaFSE-week-3/spring core maven/exercise 1/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,16 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    // Setter for Dependency Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayBook() {
+        System.out.println(bookRepository.getBookDetails());
+    }
+}
